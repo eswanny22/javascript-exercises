b6230c92acb73570cd9d6b363bb706dfdc3d7b08
Eric Swanson <ejswanson22@gmail.com>
2022-12-23 17:53:45 -0600

    repeatString pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let hey = '';
+    for (let step = 0; step < num; step++) {
+        // Runs 5 times, with values of step 0 through 4.
+        hey += string;  
+    }
+    return hey;
 };
 
 // Do not edit below this line
