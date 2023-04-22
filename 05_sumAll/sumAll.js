648aff2082e820001ca4924801bf4cd1a1a7585a
Eric Swanson <ejswanson22@gmail.com>
2023-04-22 13:45:14 -0500

    sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+
+    // convert args into array and take the highest number to use in a for loop
+    let sortedArgs = args.sort(function(a, b){return a - b});
+    let sumAll = 0;
+    let first = sortedArgs[0];
+    let last = sortedArgs.at(-1);
+    var res = args.every(function(element) {return typeof element === 'number';});
+    console.log(res);
+
+    if (first < 0 || !res) {
+        sumAll = "ERROR";
+    } else {
+        while (first <= last) {
+            sumAll += first;
+            first++;
+        }
+    }
+
+    return sumAll;
 
 };
 
